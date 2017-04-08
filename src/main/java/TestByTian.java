2bdba7c6d13be5eb902f4e8ef40f509a5a1fa8ec
田振飞 <tzf54@sina.com>
2017-04-08 10:39:54 +0800

    添加我写的第一个程序

---
+++ src/main/java/TestByTian.java
@@ -0,0 +1,9 @@
+/**
+ * Created by Administrator on 2017/4/8.
+ * @author tian
+ */
+public class TestByTian {
+    public static void main(String[] args) {
+        System.out.println("hello");
+    }
+}
